e3b5dedcd7ae2ae5ba9183fddea1abf659a577a4
Wishmastering <charsdb@gmail.com>
2023-09-11 08:20:05 -0600

    Dynanmic pagination

--- src/App.jsx
+++ src/App.jsx
@@ -1,111 +1,3 @@
-// import { useState } from "react";
-// import "./App.css";
-// import Table from "react-bootstrap/Table";
-// import Button from "react-bootstrap/Button";
-// import Modal from "react-bootstrap/Modal";
-
-// const MOCK_USERS = [
-//   { name: "carlos", age: 10, id: 0, isSelected: false },
-//   { name: "jason", age: 20, id: 1, isSelected: false },
-// ];
-
-// export default function App() {
-//   const [usersList, setUsersList] = useState(MOCK_USERS);
-
-//   const [openModal, setOpenModal] = useState(false);
-
-//   const handleDeleteUsers = () => {
-//     const updateUsersList = usersList.filter((user) => !user.isSelected);
-//     setUsersList(updateUsersList);
-//   };
-
-//   // Props for UsersTable Component
-//   const handleUsersOnChange = (users) => {
-//     setUsersList(users);
-//   };
-
-//   // Props for UsersInputModal Component
-
-//   const handleOpenModal = () => {
-//     setOpenModal(false);
-//   };
-
-//   const handleAddUser = (user) => {
-//     setUsersList((prev) => [
-//       ...prev,
-//       { name: user.name, age: user.age, id: prev.length, isSelected: false },
-//     ]);
-//   };
-
-//   return (
-//     <main className="wrapper">
-//       <h1 className="bg-danger">Nigga React 2.0</h1>
-//       <head className="button-wrapper">
-//         <Button onClick={() => setOpenModal(true)}> Add User </Button>
-//         <Button onClick={handleDeleteUsers}> Delete Selected </Button>
-//         <Button> Jumbotron users </Button>
-//       </head>
-//       <div>
-//         <UsersTable usersList={usersList} onChange={handleUsersOnChange} />
-//         <UsersInputModal
-//           openModal={openModal}
-//           onClose={handleOpenModal}
-//           addUser={handleAddUser}
-//         />
-//       </div>
-//     </main>
-//   );
-// }
-
-// function UsersTable({ usersList, onChange }) {
-//   const handleCheckboxClick = (id) => {
-//     onChange(
-//       usersList.map((user) =>
-//         user.id === id ? { ...user, isSelected: !user.isSelected } : user
-//       )
-//     );
-//   };
-
-//   return (
-//     <Table striped bordered hover>
-//       <thead>
-//         <tr>
-//           <th>Select</th>
-//           <th>Name</th>
-//           <th>Age</th>
-//           <th>Above 18</th>
-//         </tr>
-//       </thead>
-//       <tbody>
-//         {usersList.map((user) => (
-//           <UserRow
-//             user={user}
-//             handleChange={handleCheckboxClick}
-//             key={user.id}
-//           />
-//         ))}
-//       </tbody>
-//     </Table>
-//   );
-// }
-
-// function UserRow({ user, handleChange }) {
-//   return (
-//     <tr>
-//       <td>
-//         <input
-//           type="checkbox"
-//           checked={user.isSelected}
-//           onClick={() => handleChange(user.id)}
-//         />
-//       </td>
-//       <td>{user.name}</td>
-//       <td>{user.age}</td>
-//       <td>{user.age >= 18 ? "True" : "False"}</td>
-//     </tr>
-//   );
-// }
-
 import React, { useState } from "react";
 import "./App.css";
 import Table from "react-bootstrap/Table";
@@ -115,6 +7,10 @@ import Modal from "react-bootstrap/Modal";
 const MOCK_USERS = [
   { name: "carlos", age: 10, id: 0, isSelected: false },
   { name: "jason", age: 20, id: 1, isSelected: false },
+  { name: "zilean", age: 14, id: 2, isSelected: false },
+  { name: "bozo", age: 15, id: 3, isSelected: false },
+  { name: "prapra", age: 19, id: 4, isSelected: false },
+  { name: "yoyo", age: 20, id: 5, isSelected: false },
   // Add more users...
 ];
 
@@ -124,7 +20,7 @@ export default function App() {
 
   // Pagination state
   const [currentPage, setCurrentPage] = useState(1);
-  const usersPerPage = 5; // Number of users to display per page
+  const [usersPerPage, setUsersPerPage] = useState(2); // Number of users to display per page
 
   const handleDeleteUsers = () => {
     const updateUsersList = usersList.filter((user) => !user.isSelected);
@@ -149,6 +45,25 @@ export default function App() {
     setOpenModal(false); // Close the modal after adding a user
   };
 
+  const handleJumbotron = () => {
+    console.log("Jumbotron");
+  };
+
+  const handleUsersPerPage = (e) => {
+    const usersPerPage = e.target.value;
+
+    // Calculate the new number of pages based on the new number of users per page
+    const newNumberOfPages = Math.ceil(usersList.length / usersPerPage);
+
+    // If the current page exceeds the new number of pages, set it to the last page
+    if (currentPage > newNumberOfPages) {
+      setCurrentPage(newNumberOfPages);
+    }
+
+    // Update the number of users per page
+    setUsersPerPage(usersPerPage);
+  };
+
   // Calculate the users to display based on pagination
   const indexOfLastUser = currentPage * usersPerPage; // 2 * 5 = 10
   const indexOfFirstUser = indexOfLastUser - usersPerPage; // 10 - 5 = 5
@@ -160,6 +75,7 @@ export default function App() {
       <div className="button-wrapper">
         <Button onClick={handleOpenModal}>Add User</Button>
         <Button onClick={handleDeleteUsers}>Delete Selected</Button>
+        <Button onClick={handleJumbotron}>Jumbotron Selected</Button>
         {/* Pagination controls */}
         <div className="pagination">
           <Button
@@ -178,6 +94,13 @@ export default function App() {
       </div>
       <div>
         <UsersTable usersList={displayedUsers} onChange={handleUsersOnChange} />
+        <label htmlFor="users">Users Per Page:</label>
+        <select id="users" name="users" onChange={handleUsersPerPage}>
+          <option value="2">2</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+          <option value="10">10</option>
+        </select>
         <UsersInputModal
           openModal={openModal}
           onClose={() => setOpenModal(false)}
@@ -254,7 +177,7 @@ function UsersInputModal({ openModal, onClose, addUser }) {
       className="modal show"
       style={{ display: openModal ? "block" : "none", position: "absolute" }}
     >
-      <Modal.Dialog keyboard>
+      <Modal.Dialog>
         <Modal.Header closeButton>
           <Modal.Title>Add New User To Table</Modal.Title>
         </Modal.Header>
